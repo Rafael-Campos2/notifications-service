b1f5959dbc70dffd9eba0f45a2360aa0547e0c0a
Rafael Campos <rafaelsilvamz111@gmail.com>
2023-01-08 12:37:57 -0300

    feat: create notification to domain mapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@app/entities/notification';
+import { Content } from '@app/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -13,5 +15,19 @@ export class PrismaNotificationMapper {
       recipientId,
       content: content.value,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        createdAt: raw.createdAt,
+        canceledAt: raw.canceledAt,
+        readAt: raw.readAt,
+      },
+      raw.id,
+    );
   }
 }
